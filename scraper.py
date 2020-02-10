e3dfb189f4d425424792d7b0d2bf6320b7ff86c5
Kenzo Spaulding <kenzo.spaulding@gmail.com>
2020-02-09 17:45:54 -0800

    setup

--- scraper.py
+++ scraper.py
@@ -1,18 +1,36 @@
+import lxml.html as html
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag
+import urllib
 
-def scraper(url, resp):
-    links = extract_next_links(url, resp)
+
+def scraper(url: str, resp) -> list:
+    temp_links = extract_next_links(url, resp)
+    links = []
+    for link in temp_links:
+        a = urldefrag(link[2])[0]
+        links.append(a)
+    print(links)
     return [link for link in links if is_valid(link)]
 
-def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+
+def extract_next_links(url: str, resp) -> list:
+    return [link for link in html.iterlinks(resp.raw_response.content)]
+
 
 def is_valid(url):
     try:
+        def check_url(url2):
+            url2 = url2
+            valids = {".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu", ".stat.uci.edu",
+                      "today.uci.edu/department/information_computer_sciences"}
+            for entry in valids:
+                if entry in url2:
+                    return True
+            return False
+
         parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
+        if parsed.scheme not in set(["http", "https"]) or not check_url(url):
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -25,5 +43,5 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for ", parsed)
         raise
